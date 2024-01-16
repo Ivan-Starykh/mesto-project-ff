ddf80f87d1e261e67cf05873675f8b772b6d7b72
Ivan Starykh <izobilny2016@yandex.ru>
2024-01-16 07:47:48 +0300

    6 спринт

---
+++ src/components/modal.js
@@ -0,0 +1,21 @@
+// в файле modal.js описаны функции для 
+// работы с модальными окнами: функция 
+// открытия модального окна, функция 
+// закрытия модального окна, функция-обработчик 
+// события нажатия Esc и функция-обработчик
+//  события клика по оверлею;
+// Функция для открытия модального окна
+
+	// // Получаем элементы модальных окон
+	// const editModal = document.querySelector('.popup_type_edit');
+	// const addModal = document.querySelector('.popup_type_new-card');
+	// const imageModal = document.querySelector('.popup_type_image');
+
+	// // Получаем кнопки, которые открывают модальные окна
+	// const editButton = document.querySelector('.profile__edit-button');
+	// const addButton = document.querySelector('.profile__add-button');
+	// const imageButton = document.querySelector('.places__item card');
+
+	// // Получаем элементы для закрытия модальных окон
+	// const closeButtons = document.querySelectorAll('.popup__close');
+
